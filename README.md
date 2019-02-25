3f580b1673f039bc74d04afa8f5c67e64cc2e814
SanjanaNaidu <34266562+SanjanaNaidu@users.noreply.github.com>
2019-02-25 23:19:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MoSahajya-master
---
+++ app/src/main/java/com/example/vaibhav/mosahajya/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.vaibhav.mosahajya;
+
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult instanceIdResult) {
+
+                String mtoken = instanceIdResult.getToken();
+                Log.d("tokenmilgya",mtoken);
+
+            }
+        });
+    }
+}
